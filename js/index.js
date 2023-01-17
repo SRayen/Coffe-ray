2271cc3cc9a9853f7aa318f88e2c25d46b430b03
Selmen Rayen <selmanerayene95@gmail.com>
2023-01-17 18:37:26 +0100

    anim

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const btn=document.querySelector("#btn")
 const name=document.querySelector("#name")
+const result=document.querySelector("#result")
 btn.addEventListener("click",(e) => {
-    alert(`Merci ${name.value} pour votre question,nous allons vous répondre dans les meilleurs délais `)
+    e.preventDefault()
+    let text=`Merci ${name.value} pour votre question,nous allons vous répondre dans les meilleurs délais `
+    result.innerHTML=text
 })
-const myFunction= () => {
-   
-}
