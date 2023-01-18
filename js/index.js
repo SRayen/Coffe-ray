1586f0caa2c03f97cbee0c9fed02008d3d10664c
Selmen Rayen <selmanerayene95@gmail.com>
2023-01-18 12:39:22 +0100

    responsive

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 const btn=document.querySelector("#btn")
 const name=document.querySelector("#name")
 const result=document.querySelector("#result")
+
 btn.addEventListener("click",(e) => {
     e.preventDefault()
     let text=`Merci ${name.value} pour votre question,nous allons vous répondre dans les meilleurs délais `
     result.innerHTML=text
 })
+
